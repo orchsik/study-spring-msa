4e26548bcefbef2d934bb7865c6f5e452d5e668f
DODO <46145002+orchsik@users.noreply.github.com>
2024-07-20 12:09:07 +0900

    Add: partner entity 생성

---
+++ order/src/main/java/dev/practice/order/domain/partner/Partner.java
@@ -0,0 +1,63 @@
+package dev.practice.order.domain.partner;
+
+import dev.practice.order.common.util.TokenGenerator;
+import dev.practice.order.domain.AbstractEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+@Slf4j
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "partners")
+public class Partner extends AbstractEntity {
+    private final String PREFIX_PARTNER = "ptn_";
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String partnerToken;
+    private String partnerName;
+    private String businessNo;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+
+    @Builder
+    public Partner(String partnerName, String businessNo, String email) {
+        if (StringUtils.isEmpty(partnerName)) throw new RuntimeException("empty partnerName");
+        if (StringUtils.isEmpty(businessNo)) throw new RuntimeException("empty businessNo");
+        if (StringUtils.isEmpty(email)) throw new RuntimeException("empty email");
+
+        this.partnerToken = TokenGenerator.randomCharacterWithRrefix(PREFIX_PARTNER);
+        this.partnerName = partnerName;
+        this.businessNo = businessNo;
+        this.email = email;
+        this.status = Status.ENABLE;
+    }
+
+    public void enable() {
+        this.status = Status.ENABLE;
+    }
+
+    public void disabe() {
+        this.status = Status.DISABLE;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Status {
+        ENABLE("활성화"), DISABLE("비활성화");
+        private final String description;
+    }
+
+}
+
